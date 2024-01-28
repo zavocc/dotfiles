a6c18fe847a29674b45447db3b1647e0f3300155
marcusz <zavocc1030@outlook.com>
2024-01-28 19:35:19 +0800

    add starship configs

--- prep-linux.sh
+++ prep-linux.sh
@@ -3,8 +3,8 @@
 cd "$(dirname "$(realpath "$0")")"
 
 # append the .bashrc file with the contents of external bashrc
+cp linux-home/.bashrc_extern ~/.bashrc_extern
 if ! grep -qs ". ~/.bashrc_extern" ~/.bashrc; then
-    cp linux-home/.bashrc_extern ~/.bashrc_extern
     echo ". ~/.bashrc_extern" >> ~/.bashrc
 fi
 
@@ -12,3 +12,6 @@ fi
 if [ ! -d ~/.config/nvim ] || [ ! -f ~/.config/nvim/init.lua ] && [ ! -d ~/.local/share/nvim/site ]; then
     cp -r linux-home/.config/nvim ~/.config/nvim
 fi
+
+# starship
+cp linux-home/.config/starship.toml ~/.config/starship.toml
