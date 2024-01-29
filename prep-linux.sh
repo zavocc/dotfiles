08af84488aeff16feb2fd4d7f6474072aa204faa
marcusz <zavocc1030@outlook.com>
2024-01-29 19:49:02 +0800

    make the script array-style to make it easier to track files and apps

--- prep-linux.sh
+++ prep-linux.sh
@@ -2,16 +2,31 @@
 # get the realpath of this script and cd to its directory
 cd "$(dirname "$(realpath "$0")")"
 
+# Apps
+APPS=(
+    ".gitconfig"
+    ".config/starship.toml"
+    ".config/nvim"
+    ".bashrc_extern"
+)
+
+# copy files
+for x in ${APPS[@]}; do  
+    echo -e "[i] Installing apps: \e[4m$x\e[0m to \e[4m~/$x\e[0m"
+    # check if there is a .config/ prefix in the files iterated
+    if [[ $x == .config/* ]]; then
+        # check if the directory exists
+        if [ ! -d ~/.config ]; then
+            mkdir ~/.config -p
+        fi
+
+        cp linux-home/$x ~/.config -a
+    fi
+
+    cp linux-home/$x ~ -a
+done
+
 # append the .bashrc file with the contents of external bashrc
-cp linux-home/.bashrc_extern ~/.bashrc_extern
 if ! grep -qs ". ~/.bashrc_extern" ~/.bashrc; then
     echo ". ~/.bashrc_extern" >> ~/.bashrc
 fi
-
-# copy neovim config if it doesn't exist and only if there is no runtime files yet
-if [ ! -d ~/.config/nvim ] || [ ! -f ~/.config/nvim/init.lua ] && [ ! -d ~/.local/share/nvim/site ]; then
-    cp -r linux-home/.config/nvim ~/.config/nvim
-fi
-
-# starship
-cp linux-home/.config/starship.toml ~/.config/starship.toml
