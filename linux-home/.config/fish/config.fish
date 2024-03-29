e82910ab612edccc295359c989b9c7cbf723a502
marcusz <zavocc1030@outlook.com>
2024-03-30 00:37:53 +0800

    install fisher plugin when not installed

--- linux-home/.config/fish/config.fish
+++ linux-home/.config/fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
+    # Install fisher when not installed
+    not test -f ~/.config/fish/functions/fisher.fish && curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
     # Commands to run in interactive sessions can go here
     ##############################
     # Aliases
