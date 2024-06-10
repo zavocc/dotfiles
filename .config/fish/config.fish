3e8c530318bbfc399067bf553df99171280d96f3
zavocc <zavocc1030@outlook.com>
2024-06-10 15:07:53 +0800

    not using aliases for now
    its barely used, used to TAB completions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,7 @@
+# Commands to run in interactive sessions can go here
 if status is-interactive
     # Install fisher when not installed
     not test -f ~/.config/fish/functions/fisher.fish && curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-    # Commands to run in interactive sessions can go here
-    ##############################
-    # Aliases
-    ##############################
-    # Check for .aliasrc
-    [ -f ~/.aliasrc ] && source ~/.aliasrc
 
     # check if lsd is installed
     if command -q lsd
@@ -23,9 +18,6 @@ if status is-interactive
 
     # Check the presence of /opt/nvim-linux64/bin/nvim
     [ -f /opt/nvim-linux64/bin/nvim ] && set -xp PATH /opt/nvim-linux64/bin/nvim
-
-    # alias vi to neovim when available
-    command -q nvim && alias vi='nvim'
 
     # initialize starship prompt
     [ -f ~/.config/starship.toml ] && command -q starship && eval (starship init fish)
