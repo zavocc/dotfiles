26cd52ff26525fd627351ecd8bfde9212b0380ac
marcusz <zavocc1030@outlook.com>
2024-06-22 16:28:29 +0800

    nvim basic lsp setup for python

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,53 +29,5 @@ vim.keymap.set('n', '<C-n>', ':NERDTreeToggle<CR>')
 vim.keymap.set('n', '<C-Up>', ':resize +2<CR>')
 vim.keymap.set('n', '<C-Down>', ':resize -2<CR>')
 
---[[-----------------------------------------------------------------------------------
-Plugins!
------------------------------------------------------------------------------------]]--
-
--- Plugins setup
--- Check if we have git installed
-if vim.fn.executable("git") == 0 then
-  print("Git not found!")
-  return
-end
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Lazy load plugins
--- Parameters are table.... interesting
--- https://stackoverflow.com/questions/57792137/lua-functions-how-to-use-tables-as-function-arguments-and-self-document-the-cod
-require("lazy").setup{
-    "preservim/nerdtree",
-    "tpope/vim-sleuth",
-    "tpope/vim-fugitive",
-    "lewis6991/gitsigns.nvim",
-    "nvim-lualine/lualine.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "romgrk/barbar.nvim",
-    "chrisbra/changesPlugin",
-    "ntpeters/vim-better-whitespace",
-    "github/copilot.vim",
-    "folke/tokyonight.nvim",
-}
-
--- Colorscheme
-vim.cmd("colorscheme tokyonight-moon")
-
--- Initialize lualine
-require("lualine").setup{
-    options = {
-        theme = "horizon"
-    }
-}
+-- Initiate plugins
+require("plugins")
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,91 @@
+-- Plugins setup
+-- Check if we have git installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- Check if git is installed
+  if vim.fn.executable("git") == 0 then
+    print("Git not found!")
+    return
+  end
+  -- Install lazy
+  vim.fn.system({
+    "git","clone","--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+-- Lazy load plugins
+-- Parameters are table.... interesting
+-- https://stackoverflow.com/questions/57792137/lua-functions-how-to-use-tables-as-function-arguments-and-self-document-the-cod
+local lsp_plugins = {
+    "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+}
+
+require("lazy").setup{
+    "preservim/nerdtree",
+    "tpope/vim-sleuth",
+    "tpope/vim-fugitive",
+    "nvim-lualine/lualine.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "romgrk/barbar.nvim",
+    "chrisbra/changesPlugin",
+    "ntpeters/vim-better-whitespace",
+    "folke/tokyonight.nvim",
+    lsp_plugins
+}
+
+-- Colorscheme
+vim.cmd("colorscheme tokyonight-moon")
+
+-- Initialize lualine
+require("lualine").setup{
+    options = {
+        theme = "horizon"
+    }
+}
+
+-- Setup LSP
+require'lspconfig'.pyright.setup{}
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require('lspconfig')
+
+lspconfig.pyright.setup{
+  capabilities = capabilities,
+}
+
+-- luasnip setup
+local luasnip = require("luasnip")
+
+-- nvim-cmp setup
+local cmp = require("cmp")
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  -- Enable in-line suggestions
+  experimental = { ghost_text = true },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    -- Prefer tab to accept completion
+    ['<Tab>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
