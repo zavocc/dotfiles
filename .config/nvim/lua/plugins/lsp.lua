3343051b413369e5f270fdb8deb069eb0527ff08
marcusz <zavocc1030@outlook.com>
2024-07-08 15:56:55 +0800

    change mappings for cmp, and do not lazy load everything by default
    Since lazy loading loads them on demand (when needed) and may not load all plugins immediately
    can be observed by `setting config.defaults.lazy = true` in `init.lua` lazy.nvim setup
    
    Lazy loading should only be used when there is a plugin causes performance hit on startup and
    only load them when either commands, events, or a plugin needs to load its only dependencies immediately or VeryLazy
    - https://www.reddit.com/r/neovim/comments/uwndk0/do_you_use_lazy_loading_and_why/
    - https://www.reddit.com/r/neovim/comments/136k01s/what_are_plugins_that_should_not_be_lazy_loaded/
    - https://lazy.folke.io/spec/lazy_loading
    
    VeryLazy event:
    - https://github.com/folke/lazy.nvim/discussions/518

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -49,11 +49,11 @@ return {
                 ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
                 -- C-b (back) C-f (forward) for snippet placeholder navigation.
                 ['<C-Space>'] = cmp.mapping.complete(),
-                ['<CR>'] = cmp.mapping.confirm {
+                ['<Tab>'] = cmp.mapping.confirm {
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = true,
                 },
-                ['<Tab>'] = cmp.mapping(function(fallback)
+                ['<Down>'] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_next_item()
                 elseif luasnip.expand_or_jumpable() then
@@ -62,7 +62,7 @@ return {
                     fallback()
                 end
                 end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                ['<Up>'] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_prev_item()
                 elseif luasnip.jumpable(-1) then
@@ -81,7 +81,6 @@ return {
     },
     {
         "ray-x/lsp_signature.nvim",
-        event = "VeryLazy",
         config = function()
                 vim.api.nvim_create_autocmd("LspAttach", {
                 callback = function(args)
--- .config/nvim/lua/plugins/theming.lua
+++ .config/nvim/lua/plugins/theming.lua
@@ -11,7 +11,6 @@ return {
     },
     {
         "folke/tokyonight.nvim",
-        lazy = false,
         priority = 1000,
         config = function()
             -- load the colorscheme here
