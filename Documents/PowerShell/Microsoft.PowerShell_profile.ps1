2b0dbd6441e406c4c910ddc2cb1104137e133023
Wyatt Marcus Bautista <zavocc1030@outlook.com>
2024-08-24 23:19:54 +0800

    powershell: Use proper way to check if starship is installed

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-$STARSHIP_PATH="C:\Users\zavoc\Starship\starship.exe"
+$STARSHIP_PATH=(Get-Command -Name starship.exe  -CommandType Application).Source
 
 if (Test-Path -Path $STARSHIP_PATH) {
     Invoke-Expression (& $STARSHIP_PATH init powershell --print-full-init | Out-String)
