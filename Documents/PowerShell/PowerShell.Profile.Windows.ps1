9d46163e51eb8a3b53928e2acddff6e7bdeb50e0
Wyatt Marcus Bautista <zavocc1030@outlook.com>
2024-08-26 11:45:08 +0800

    adjdjaasjdoadjpsoad3r

--- Documents/PowerShell/PowerShell.Profile.Windows.ps1
+++ Documents/PowerShell/PowerShell.Profile.Windows.ps1
@@ -7,5 +7,5 @@ if (Test-Path -Path $STARSHIP_PATH) {
 # Check if we have FHM installed
 $FNM_PATH=(Get-Command -Name fnm.exe  -CommandType Application).Source
 if (Test-Path -Path $FNM_PATH) {
-    Invoke-Expression (& $FNM_PATH env --use-on-cd | Out-String | Invoke-Expression)
+    Invoke-Expression (& $FNM_PATH env --use-on-cd | Out-String)
 }
