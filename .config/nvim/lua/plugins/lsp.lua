2fd348e4b12a929ecf8dce3bb6ab7908956227b8
WMCB Tech (marcusz) <zavocc1030@outlook.com>
2024-10-06 13:24:41 +0000

    Lazy load nvim cmp and lsp signature only after lspconfig loads

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,10 @@ return {
     -- LSPs
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/nvim-cmp",
+            "ray-x/lsp_signature.nvim"
+        },
         config = function()
             local pyright_capabilities = require("cmp_nvim_lsp").default_capabilities()
             require("lspconfig").pyright.setup{
@@ -35,60 +39,60 @@ return {
             require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup {
-            -- Snippet
-            snippet = {
-                expand = function(args)
-                require('luasnip').lsp_expand(args.body)
-                end,
-            },
-            -- Rounded borders
-            window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
-            -- Enable in-line suggestions
-            experimental = { ghost_text = true },
-            -- Icons
-            formatting = {
-                format = lspkind.cmp_format({
-                    mode = 'symbol',
-                    maxwidth = 30,
-                    ellipsis_char = '...',
-                    show_labelDetails = false,
-                })
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-                ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-                -- C-b (back) C-f (forward) for snippet placeholder navigation.
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<Tab>'] = cmp.mapping.confirm {
-                behavior = cmp.ConfirmBehavior.Replace,
-                select = true,
+                -- Snippet
+                snippet = {
+                    expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                    end,
                 },
-                ['<Down>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item()
-                elseif luasnip.expand_or_jumpable() then
-                    luasnip.expand_or_jump()
-                else
-                    fallback()
-                end
-                end, { 'i', 's' }),
-                ['<Up>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
-                else
-                    fallback()
-                end
-                end, { 'i', 's' }),
-            }),
-            sources = {
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-            },
+                -- Rounded borders
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                -- Enable in-line suggestions
+                experimental = { ghost_text = true },
+                -- Icons
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'symbol',
+                        maxwidth = 30,
+                        ellipsis_char = '...',
+                        show_labelDetails = false,
+                    })
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+                    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<Tab>'] = cmp.mapping.confirm {
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                    },
+                    ['<Down>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                    end, { 'i', 's' }),
+                    ['<Up>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                    end, { 'i', 's' }),
+                }),
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                },
             }
         end
     },
