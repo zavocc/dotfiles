67a1adab72a292a57304b91aada753e96768bf94
zavocc <buffer.0verfl0w1030@gmail.com>
2025-10-13 19:55:00 +0800

    Finally nvim-tree works but i didn't setup keybindings yet

--- .config/nvim/lua/plugins/plugs.lua
+++ .config/nvim/lua/plugins/plugs.lua
@@ -1,4 +1,18 @@
 return {
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        -- Please see https://lazy.folke.io/spec#spec-setup
+        -- :NvimTree* editor commands won't be populated until config is set to true or
+        -- manual setup is ran
+        config = true
+        -- or 
+        -- config = function()
+        --    require("nvim-tree").setup({})
+        -- end,
+    },
     "tpope/vim-sleuth",
     "chrisbra/changesPlugin",
     {
